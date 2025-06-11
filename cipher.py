5afc37385c3afe73d964850918ce26b159155c2e
obirikan <obirikan020@gmail.com>
2025-06-11 16:41:32 +0000

    decoder problem

--- cipher.py
+++ cipher.py
@@ -1,6 +1,6 @@
 from Tokenizer.main import Transfomer
 
-input_sentence = 'call me on my number'
+input_sentence = 'jxu evviuj veh jxu iusedt cuiiqwu yi vekhjuud.' # decoder problem 
 input_sentence = input_sentence.lower()
 
 encoded_message,decoded_message=Transfomer(input_sentence)
