597e4d450dd712d57a790794a758db422c8f0e82
obirikan <obirikan020@gmail.com>
2025-06-11 15:05:02 +0000

    PHASE 1

--- Tokenizer/main.py
+++ Tokenizer/main.py
@@ -2,12 +2,12 @@ from decoder.decoder import Decoded_Message
 from encoder.encoder import Encode_Message
 from variables.main import alphabets
 def Transfomer(input_sentence):
-    #Getting every single letter in the sentence
+    
     each_letter=[]
     for word in input_sentence:
         each_letter.append(word)
 
-    #finding each index(our input) in the alphabet 
+    
     word_index_in_alphabet=[alphabets.find(l) for l in each_letter]
     
     encoded_message=Encode_Message(each_letter)
