195014673f9f6433dbbb39dbf998794d7505305b
obirikan <obirikan020@gmail.com>
2025-06-13 12:23:19 +0000

    updated diagram

--- cipher.py
+++ cipher.py
@@ -1,6 +1,6 @@
 from Tokenizer.main import Transformer
 
-input_sentence = 'xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!'
+input_sentence = 'hello world'
 encoded, decoded = Transformer(input_sentence)
 print(f"Original: {input_sentence}")
 print(f"Encoded: {encoded}")
